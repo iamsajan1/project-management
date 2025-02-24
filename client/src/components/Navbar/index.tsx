0ed359f91cd25fda30e8150f111f43f98e566c6f
iamsajan1 <kumarsantosh.iict06a@gmail.com>
2025-02-24 06:25:22 +0530

    added corrected side bar corrected

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -76,7 +76,7 @@ console.error("error sign out", error);
           <div className="align-center flex h-9 w-9 justify-center">
             {!!currentUserDetails?.profilePictureUrl ? (
               <Image
-                src={`https://pm-s3-images.s3.us-east-2.amazonaws.com/${currentUserDetails?.profilePictureUrl}`}
+                src={`https://prom-s3-images.s3.us-east-1.amazonaws.com/${currentUserDetails?.profilePictureUrl}`}
                 alt={currentUserDetails?.username || "User Profile Picture"}
                 width={100}
                 height={50}
